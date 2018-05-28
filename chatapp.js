4df4385de5969eeb433a12e6e285b2ddc9eb09a1
Josh Ladwig <joshua_ladwig@yahoo.com>
2018-05-28 19:58:02 +0000

    trying to add username functionality

--- chatapp.js
+++ chatapp.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
 const nunjucks = require('nunjucks');
+const body_parser = require('body-parser');
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 app.use(express.static('public'));
+app.use(body_parser.urlencoded({extended: false}));
 
 nunjucks.configure('views', {
   autoescape: true,
@@ -11,14 +13,47 @@ nunjucks.configure('views', {
   noCache: true
 });
 
+var nicknames = {};
 
 app.get('/', function(req, resp){
+    resp.render('base.html', {});
+});
+
+
+app.get('/chat', function(req, resp){
     resp.render('chat.html', {});
 });
 
-io.on('connection', function(socket){
-    console.log('a user connected');
-    socket.on('chat message', function(msg){
+app.post('/chat', function(req, resp){
+    // //get the nickname of the request user test for validness at some point
+    // var userName = req.body.nickname;
+    // //send the nicknames array and do a for loop to display who's online.
+    // resp.render('chat.html', {nickname: userName});
+    resp.redirect('chat.html');
+});
+
+
+io.on('connection', function(client){
+    
+    //when I create a connection log my id to my username?
+    client.broadcast.emit('broadcast', 'a new user has joined');
+    
+    client.on('data', function(data){
+        nicknames[client.id] = {name: data};
+        console.log('nicknames: ', nicknames);
+    })
+    
+    client.on('disconnect', function() {
+        var msg = 'user has disconnected'
+        io.emit('disconnect', msg);
+    });
+    
+    client.on('user', function(msg) {
+        console.log(msg);
+    });
+        
+    //server side socket -> server EventEmitter
+    client.on('chat message', function(msg){
         io.emit('chat message', msg);
     });
 });
--- public/js/chat.js
+++ public/js/chat.js
@@ -1,12 +1,36 @@
+//CLIENT
+
 
 $(function () {
+    //client side socket -> client EventEmitter
     var socket = io();
+    
+    $('#send').on('click', function(){
+       socket.emit('data', $('#nickname').val());
+       return false;
+    });
+    
     $('form').submit(function(){
+        //this line is just sending our message from 'form' back to server
         socket.emit('chat message', $('#m').val());
+        //next line clears the text box for next message.
         $('#m').val('');
         return false;
     });
+    
     socket.on('chat message', function(msg){
+        var $nameMsg = $('#nickname').text() + ' says: ';
+        console.log($nameMsg);
         $('#messages').append($('<li>').text(msg));
     });
+    
+    socket.on('broadcast', function(msg) {
+      $('#rmMsgs').append($('<li>').text(msg));
+    });
+    
+    socket.on('disconnect', function(msg){
+        console.log($('#nickname').val());
+        $('#rmMsgs').append($('<li>').text(msg));
+    });
+    
 });
